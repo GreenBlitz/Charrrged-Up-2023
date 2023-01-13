1c7b4052fe51b878fd1612d634c091277fd87cd9
Raz <razrishoni135@gmail.com>
2023-01-13 14:26:43 +0200

    Romy and Raz - minor refactoring

--- src/main/java/edu/greenblitz/tobyDetermined/commands/swerve/MoveToPos.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/swerve/MoveToPos.java
@@ -6,28 +6,28 @@ import edu.greenblitz.utils.GBCommand;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 
-public class MoveToPos extends GBCommand{
-	private ProfiledPIDController xController;
-	private ProfiledPIDController yController;
-	private ProfiledPIDController rotationController;
+public class MoveToPos extends GBCommand {
+    private ProfiledPIDController xController;
+    private ProfiledPIDController yController;
+    private ProfiledPIDController rotationController;
 
-	public MoveToPos(Pose2d pos){
-		this.xController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.translationKp, RobotMap.Swerve.Autonomus.translationKi, RobotMap.Swerve.Autonomus.translationKd, RobotMap.Swerve.Autonomus.constraints);
-		xController.setGoal(pos.getX());
-		this.yController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.translationKp, RobotMap.Swerve.Autonomus.translationKi, RobotMap.Swerve.Autonomus.translationKd, RobotMap.Swerve.Autonomus.constraints);
-		yController.setGoal(pos.getY());
-		this.rotationController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.rotKp, RobotMap.Swerve.Autonomus.rotKi, RobotMap.Swerve.Autonomus.rotKd, RobotMap.Swerve.Autonomus.constraints);
-		rotationController.setGoal(pos.getRotation().getRadians());
-		rotationController.enableContinuousInput(0, 2*Math.PI);
-	}
+    public MoveToPos(Pose2d pos) {
+        xController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.translationKp, RobotMap.Swerve.Autonomus.translationKi, RobotMap.Swerve.Autonomus.translationKd, RobotMap.Swerve.Autonomus.constraints);
+        xController.setGoal(pos.getX());
+        yController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.translationKp, RobotMap.Swerve.Autonomus.translationKi, RobotMap.Swerve.Autonomus.translationKd, RobotMap.Swerve.Autonomus.constraints);
+        yController.setGoal(pos.getY());
+        rotationController = new ProfiledPIDController(RobotMap.Swerve.Autonomus.rotKp, RobotMap.Swerve.Autonomus.rotKi, RobotMap.Swerve.Autonomus.rotKd, RobotMap.Swerve.Autonomus.constraints);
+        rotationController.setGoal(pos.getRotation().getRadians());
+        rotationController.enableContinuousInput(0, 2 * Math.PI);
+    }
 
-	@Override
-	public void execute() {
-		SwerveChassis.getInstance().moveByChassisSpeeds(
-				xController.calculate(SwerveChassis.getInstance().getRobotPose().getX()),
-				yController.calculate(SwerveChassis.getInstance().getRobotPose().getY()),
-				rotationController.calculate(SwerveChassis.getInstance().getRobotPose().getRotation().getRadians()),
-				SwerveChassis.getInstance().getChassisAngle()
-		);
-	}
+    @Override
+    public void execute() {
+        SwerveChassis.getInstance().moveByChassisSpeeds(
+                xController.calculate(SwerveChassis.getInstance().getRobotPose().getX()),
+                yController.calculate(SwerveChassis.getInstance().getRobotPose().getY()),
+                rotationController.calculate(SwerveChassis.getInstance().getRobotPose().getRotation().getRadians()),
+                SwerveChassis.getInstance().getChassisAngle()
+        );
+    }
 }
