e84294d7653fa1495e8988aec4652f9baf7eb1b6
NoamRosenberg08 <noam.rosenberg.private@gmail.com>
2023-02-09 18:52:04 +0200

    noam - solved some moreeee

--- src/main/java/edu/greenblitz/tobyDetermined/OI.java
+++ src/main/java/edu/greenblitz/tobyDetermined/OI.java
@@ -1,57 +1,56 @@
 package edu.greenblitz.tobyDetermined;
 
 
-import edu.greenblitz.tobyDetermined.commands.LED.DefaultColor;
-import edu.greenblitz.tobyDetermined.commands.LED.ObjectInClawLED;
-import edu.greenblitz.tobyDetermined.commands.LED.ObjectInIntakeLED;
-import edu.greenblitz.tobyDetermined.commands.LED.HumanPlayerObjectIndicator;
+import edu.greenblitz.tobyDetermined.commands.swerve.CombineJoystickMovement;
+import edu.greenblitz.tobyDetermined.commands.swerve.MoveByVisionSupplier;
+import edu.greenblitz.tobyDetermined.subsystems.swerve.SwerveChassis;
 import edu.greenblitz.utils.hid.SmartJoystick;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class OI { //GEVALD
-	
-	private static OI instance;
-	private static boolean isHandled = true;
-	private final SmartJoystick mainJoystick;
-	
-	private final SmartJoystick secondJoystick;
-	
-	
-	private OI() {
-		mainJoystick = new SmartJoystick(RobotMap.Joystick.MAIN, 0.1);
-		secondJoystick = new SmartJoystick(RobotMap.Joystick.SECOND, 0.2);
-		initButtons();
-		
-	}
-	
-	public static OI getInstance() {
-		if (instance == null) {
-			instance = new OI();
-		}
-		return instance;
-	}
-	
-	public static boolean isIsHandled() {
-		return isHandled;
-	}
-	
-	public static void disableHandling() {
-		isHandled = false;
-	}
-	
-	private void initButtons() {
 
-		mainJoystick.B.whileTrue(new ObjectInIntakeLED());
-		mainJoystick.Y.whileTrue(new ObjectInClawLED());
-		mainJoystick.X.whileTrue(new HumanPlayerObjectIndicator(HumanPlayerObjectIndicator.wantedObject.CUBE));
-		mainJoystick.A.whileTrue(new HumanPlayerObjectIndicator(HumanPlayerObjectIndicator.wantedObject.CONE));
-		mainJoystick.POV_DOWN.whileTrue(new DefaultColor());
-	}
-	
-	public SmartJoystick getMainJoystick() {
-		return mainJoystick;
-	}
-	public SmartJoystick getSecondJoystick() {
-		return secondJoystick;
-	}
-	
+    private static OI instance;
+    private static boolean isHandled = true;
+    private final SmartJoystick mainJoystick;
+
+    private final SmartJoystick secondJoystick;
+
+
+    private OI() {
+        mainJoystick = new SmartJoystick(RobotMap.Joystick.MAIN, 0.1);
+        secondJoystick = new SmartJoystick(RobotMap.Joystick.SECOND, 0.2);
+        initButtons();
+
+    }
+
+    public static OI getInstance() {
+        if (instance == null) {
+            instance = new OI();
+        }
+        return instance;
+    }
+
+    public static boolean isIsHandled() {
+        return isHandled;
+    }
+
+    public static void disableHandling() {
+        isHandled = false;
+    }
+
+    private void initButtons() {
+		SwerveChassis.getInstance().setDefaultCommand(new CombineJoystickMovement(true));
+		mainJoystick.X.whileTrue(new MoveByVisionSupplier(true));
+		mainJoystick.Y.onTrue(new InstantCommand(() -> SwerveChassis.getInstance().resetChassisPose()));
+		mainJoystick.POV_UP.onTrue(new InstantCommand(() -> SwerveChassis.getInstance().resetAllEncoders()));
+    }
+
+    public SmartJoystick getMainJoystick() {
+        return mainJoystick;
+    }
+
+    public SmartJoystick getSecondJoystick() {
+        return secondJoystick;
+    }
+
 }
