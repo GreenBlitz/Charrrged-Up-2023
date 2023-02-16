54041f77e1570450551448be83fb05e885aaee32
Raz <razrishoni135@gmail.com>
2023-02-16 23:57:11 +0200

    tal - removed everything from regular dashboard

--- src/main/java/edu/greenblitz/tobyDetermined/subsystems/Dashboard.java
+++ src/main/java/edu/greenblitz/tobyDetermined/subsystems/Dashboard.java
@@ -36,26 +36,6 @@ public class Dashboard extends GBSubsystem {
 	@Override
 	public void periodic() {
 
-//        SmartDashboard.putNumber("FR-angle-neo", Math.IEEEremainder(Math.toDegrees(SwerveChassis.getInstance().getModuleAngle(SwerveChassis.Module.FRONT_RIGHT)), 360));
-//        SmartDashboard.putNumber("FL-angle-neo", Math.IEEEremainder(Math.toDegrees(SwerveChassis.getInstance().getModuleAngle(SwerveChassis.Module.FRONT_LEFT)), 360));
-//        SmartDashboard.putNumber("BR-angle-neo", Math.IEEEremainder(Math.toDegrees(SwerveChassis.getInstance().getModuleAngle(SwerveChassis.Module.BACK_RIGHT)), 360));
-//        SmartDashboard.putNumber("BL-angle-neo", Math.IEEEremainder(Math.toDegrees(SwerveChassis.getInstance().getModuleAngle(SwerveChassis.Module.BACK_LEFT)), 360));
-//        SmartDashboard.putNumber("y", SwerveChassis.getInstance().getRobotPose().getY());
-//        SmartDashboard.putNumber("x", SwerveChassis.getInstance().getRobotPose().getX());
-//        SmartDashboard.putNumber("angle", SwerveChassis.getInstance().getRobotPose().getRotation().getDegrees());
-//        SmartDashboard.putNumber("pigeon angle", Units.radiansToDegrees(SwerveChassis.getInstance().getPigeonGyro().getYaw()));
-//
-//        SmartDashboard.putNumber("chassis speed x", SwerveChassis.getInstance().getChassisSpeeds().vxMetersPerSecond);
-//        SmartDashboard.putNumber("FR-angle-absolute", SwerveChassis.getInstance().getModuleAbsoluteEncoderValue(SwerveChassis.Module.FRONT_RIGHT));
-//        SmartDashboard.putNumber("FL-angle-absolute", SwerveChassis.getInstance().getModuleAbsoluteEncoderValue(SwerveChassis.Module.FRONT_LEFT));
-//        SmartDashboard.putNumber("BR-angle-absolute", SwerveChassis.getInstance().getModuleAbsoluteEncoderValue(SwerveChassis.Module.BACK_RIGHT));
-//        SmartDashboard.putNumber("BL-angle-absolute", SwerveChassis.getInstance().getModuleAbsoluteEncoderValue(SwerveChassis.Module.BACK_LEFT));
-//
-//        SmartDashboard.putString("pose", SwerveChassis.getInstance().getRobotPose().toString());
-//
-//		SmartDashboard.putNumber("grid pos id", Grid.getInstance().getSelectedPositionID());
-//		SmartDashboard.putString("grid pos", Grid.getInstance().getSelectedPosition().toString());
-
 	}
 
 
