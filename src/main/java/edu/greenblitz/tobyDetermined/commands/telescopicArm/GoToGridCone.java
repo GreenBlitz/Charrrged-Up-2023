9d74068e367f435e10d753018c76d6ff5e1df37b
Raz <razrishoni135@gmail.com>
2023-02-21 20:45:49 +0200

    tal - merged master into Claw-neo, and fixed some of asaf's cod

--- src/main/java/edu/greenblitz/tobyDetermined/commands/telescopicArm/GoToGridCone.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/telescopicArm/GoToGridCone.java
@@ -1,8 +1,9 @@
 package edu.greenblitz.tobyDetermined.commands.telescopicArm;
 
 import edu.greenblitz.tobyDetermined.commands.swerve.MoveToGrid.Grid;
+import edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition.GoToPosition;
 
-public class GoToGridCone extends GoToPosition{
+public class GoToGridCone extends GoToPosition {
 
 	public GoToGridCone() {
 		super(Grid.getInstance().getSelectedConePosition());
