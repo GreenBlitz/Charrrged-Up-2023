8aec5cb333b61690d32f1245ddaadd6c1c6b9c8c
greenblitz4590 <greenblitz4590@gmail.com>
2023-02-22 22:16:40 +0200

    tal -fixed romys mistakes for arm pos in master and added hail command

---
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/telescopicArm/goToPosition/ZigHail.java
@@ -0,0 +1,9 @@
+package edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition;
+
+import edu.greenblitz.tobyDetermined.RobotMap;
+
+public class ZigHail extends GoToPosition{
+    public ZigHail() {
+        super(RobotMap.TelescopicArm.PresetPositions.PRE_GRID);
+    }
+}
