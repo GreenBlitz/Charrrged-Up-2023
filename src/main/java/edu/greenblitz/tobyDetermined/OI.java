5f9d98b50626cfac22fb813ddeee44979837b43d
itamaroryan <itamar.oryan@gmail.com>
2023-02-27 16:06:37 +0200

    tal - some buttons mostly

--- src/main/java/edu/greenblitz/tobyDetermined/OI.java
+++ src/main/java/edu/greenblitz/tobyDetermined/OI.java
@@ -3,6 +3,7 @@ package edu.greenblitz.tobyDetermined;
 import edu.greenblitz.tobyDetermined.commands.rotatingBelly.RotateByPower;
 import com.revrobotics.CANSparkMax;
 import edu.greenblitz.tobyDetermined.commands.swerve.MoveToGrid.*;
+import edu.greenblitz.tobyDetermined.commands.swerve.balance.LockWheels;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.RewritePresetPosition;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.claw.EjectFromClaw;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.claw.GripCone;
@@ -11,6 +12,7 @@ import edu.greenblitz.tobyDetermined.commands.telescopicArm.elbow.*;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.extender.*;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition.GoToGrid;
 import edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition.GoToPosition;
+import edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition.ZigHail;
 import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.Elbow;
 import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.Extender;
 import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.ObjectSelector;
@@ -68,18 +70,13 @@ public class OI { //GEVALD
         secondJoystick.POV_RIGHT.onTrue(new MoveSelectedTargetRight());
         secondJoystick.POV_UP.onTrue(new MoveSelectedTargetUp());
         secondJoystick.POV_DOWN.onTrue(new MoveSelectedTargetDown());
-        secondJoystick.X.whileTrue(new GoToGrid());
-        secondJoystick.A.whileTrue(new RotateByPower(0.9));
-        secondJoystick.POV_LEFT.onTrue(new MoveSelectedTargetLeft());
-        secondJoystick.POV_RIGHT.onTrue(new MoveSelectedTargetRight());
-        secondJoystick.POV_UP.onTrue(new MoveSelectedTargetUp());
-        secondJoystick.POV_DOWN.onTrue(new MoveSelectedTargetDown());
         secondJoystick.A.whileTrue(new GoToGrid());
-        secondJoystick.B.whileTrue(new GoToPosition(RobotMap.TelescopicArm.PresetPositions.INTAKE_GRAB_POSITION));
+        secondJoystick.B.and(secondJoystick.A.negate()).whileTrue(new GoToPosition(RobotMap.TelescopicArm.PresetPositions.INTAKE_GRAB_POSITION));
         secondJoystick.X.onTrue(new InstantCommand(ObjectSelector::flipSelection));
         secondJoystick.Y.whileTrue(new EjectFromClaw());
         secondJoystick.START.whileTrue(new GripCone());
         secondJoystick.BACK.whileTrue(new GripCube());
+        secondJoystick.B.and(secondJoystick.A).whileTrue(new ZigHail());
 
 
 
@@ -88,21 +85,16 @@ public class OI { //GEVALD
 
     public void romyButtons() {
         SwerveChassis.getInstance().setDefaultCommand(new CombineJoystickMovement(true));
-        mainJoystick.X.onTrue(new RotateToAngleRadians(Math.toRadians(30)));
-        mainJoystick.B.onTrue(new RotateToAngleRadians(Math.toRadians(45)));
-        mainJoystick.A.onTrue(new RotateToAngleRadians(Math.toRadians(60)));
         mainJoystick.START.onTrue(new InstantCommand(() -> Extender.getInstance().resetLength()));
         
-//        mainJoystick.R1.whileTrue(new CombineJoystickMovement(true)); //slow mode
-//        mainJoystick.L1.whileTrue(new MoveToGrid()); //move to pose
-//        mainJoystick.POV_UP.whileTrue(new AdvancedBalanceOnRamp(true)); //ramp from community
-//        mainJoystick.POV_DOWN.whileTrue(new AdvancedBalanceOnRamp(false)); //ramp form not community
-//        mainJoystick.POV_LEFT.whileTrue(new DriveSidewaysUntilEdge(DriveSidewaysUntilEdge.Direction.LEFT, 0.5)); //left movement
-//        mainJoystick.POV_RIGHT.whileTrue(new DriveSidewaysUntilEdge(DriveSidewaysUntilEdge.Direction.RIGHT, 0.5)); //right movement
-//        mainJoystick.B.onTrue(new LockWheels()); //lock wheel
-////		mainJoystick.START.toggleOnTrue(new InstantCommand()); //todo - toggle leg
-//        mainJoystick.Y.onTrue(new InstantCommand(() -> SwerveChassis.getInstance().resetChassisPose())).and(() -> mainJoystick.R1.getAsBoolean()); //reset pose
-//        mainJoystick.X.and(mainJoystick.R1).onTrue(new InstantCommand(()-> SwerveChassis.getInstance().resetEncodersByCalibrationRod()));
+        mainJoystick.R1.whileTrue(new CombineJoystickMovement(true)); //slow mode
+        mainJoystick.L1.whileTrue(new MoveToGrid()); //move to pose
+        mainJoystick.POV_LEFT.whileTrue(new DriveSidewaysUntilEdge(DriveSidewaysUntilEdge.Direction.LEFT, 0.5)); //left movement
+        mainJoystick.POV_RIGHT.whileTrue(new DriveSidewaysUntilEdge(DriveSidewaysUntilEdge.Direction.RIGHT, 0.5)); //right movement
+        mainJoystick.B.onTrue(new LockWheels()); //lock wheel
+//		mainJoystick.START.toggleOnTrue(new InstantCommand()); //todo - toggle leg
+        mainJoystick.Y.onTrue(new InstantCommand(() -> SwerveChassis.getInstance().resetChassisPose())).and(() -> mainJoystick.R1.getAsBoolean()); //reset pose
+        mainJoystick.X.and(mainJoystick.R1).onTrue(new InstantCommand(()-> SwerveChassis.getInstance().resetEncodersByCalibrationRod()));
 
         // reset chassis pose (Y)
         mainJoystick.Y.onTrue(new InstantCommand(() -> SwerveChassis.getInstance().resetChassisPose())).and(() -> mainJoystick.R1.getAsBoolean()); //reset pose
