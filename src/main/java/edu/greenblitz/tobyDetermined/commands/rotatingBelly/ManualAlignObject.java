f5d34642bb921c9b18673206d332ce0b5d3bd18d
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2023-02-28 15:05:27 +0200

    asaf - use waitcommand instead of waituntilcommand

--- src/main/java/edu/greenblitz/tobyDetermined/commands/rotatingBelly/ManualAlignObject.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/rotatingBelly/ManualAlignObject.java
@@ -1,12 +1,13 @@
 package edu.greenblitz.tobyDetermined.commands.rotatingBelly;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 public class ManualAlignObject extends SequentialCommandGroup {
 
 	public ManualAlignObject(){
-		addCommands(new RotateOutDoorDirection().raceWith(new WaitUntilCommand(2)), new RotateInDoorDirection().raceWith(new WaitUntilCommand(2)));
+		addCommands(new RotateOutDoorDirection().raceWith(new WaitCommand(2)), new RotateInDoorDirection().raceWith(new WaitCommand(2)));
 	}
 
 }
