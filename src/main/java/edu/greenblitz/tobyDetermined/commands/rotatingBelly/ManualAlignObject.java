603941041a915b58aa14de0c7cd5c52b554d2cb3
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2023-02-28 15:15:33 +0200

    Amir - added manuel control to belly

--- src/main/java/edu/greenblitz/tobyDetermined/commands/rotatingBelly/ManualAlignObject.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/rotatingBelly/ManualAlignObject.java
@@ -1,5 +1,6 @@
 package edu.greenblitz.tobyDetermined.commands.rotatingBelly;
 
+import edu.greenblitz.tobyDetermined.RobotMap;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -7,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 public class ManualAlignObject extends SequentialCommandGroup {
 
 	public ManualAlignObject(){
-		addCommands(new RotateOutDoorDirection().raceWith(new WaitCommand(2)), new RotateInDoorDirection().raceWith(new WaitCommand(2)));
+		addCommands(new RotateInDoorDirection().raceWith(new WaitCommand(RobotMap.RotatingBelly.ROTATING_TIME)), new RotateOutDoorDirection().raceWith(new WaitCommand(RobotMap.RotatingBelly.ROTATING_TIME)));
 	}
 
 }
