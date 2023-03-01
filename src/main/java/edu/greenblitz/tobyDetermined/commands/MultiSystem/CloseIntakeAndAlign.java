fd36eeee082cc0c44c87cbbdbb4673400ec172b1
itamaroryan <itamar.oryan@gmail.com>
2023-03-01 11:09:29 +0200

    tal et asaf - added rotateBelly for more time in intake

--- src/main/java/edu/greenblitz/tobyDetermined/commands/MultiSystem/CloseIntakeAndAlign.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/MultiSystem/CloseIntakeAndAlign.java
@@ -2,6 +2,8 @@ package edu.greenblitz.tobyDetermined.commands.MultiSystem;
 
 import edu.greenblitz.tobyDetermined.commands.intake.extender.RetractRoller;
 import edu.greenblitz.tobyDetermined.commands.rotatingBelly.ManualAlignObject;
+import edu.greenblitz.tobyDetermined.commands.rotatingBelly.RotateInDoorDirection;
+import edu.greenblitz.tobyDetermined.commands.rotatingBelly.RotateOutDoorDirection;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
@@ -10,7 +12,7 @@ public class CloseIntakeAndAlign extends ParallelCommandGroup {
     public CloseIntakeAndAlign() {
         super(
                 new RetractRoller(),
-                new WaitCommand(2).andThen(new ManualAlignObject())
+                new WaitCommand(2).deadlineWith(new RotateOutDoorDirection()).andThen(new ManualAlignObject())
         );
     }
 
