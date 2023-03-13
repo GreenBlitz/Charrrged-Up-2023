e30bd1f736e98a74768f40b46a78b839236b5a74
itamaroryan <itamar.oryan@gmail.com>
2023-03-13 12:55:41 +0200

    asaf - works a bit slow

--- src/main/java/edu/greenblitz/tobyDetermined/commands/Auto/balance/bangBangBalance/BangBangBalance.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/Auto/balance/bangBangBalance/BangBangBalance.java
@@ -7,11 +7,15 @@ public class BangBangBalance extends SwerveCommand {
 
     private double originalSpeed;
     private double usedSpeed;
-    private final double USEDSPEED_FACTOR = 0.22;
-    public static final double TOLERANCE = Math.toRadians(7);
+    private final double USEDSPEED_FACTOR = 0.32;
+    public static final double TOLERANCE = Math.toRadians(13);
+    
+    public static final double FINISH_TOLERANCE = Math.toRadians(7);
+    
+    private double usedTolerance;
 
     private Debouncer debouncer;
-    private final double DEBOUNCE_TIME = 2;
+    private final double DEBOUNCE_TIME = 5;
 
     double pitchAngle;
     private boolean forwards;
@@ -26,15 +30,17 @@ public class BangBangBalance extends SwerveCommand {
         pitchAngle = 0;
         debouncer = new Debouncer(DEBOUNCE_TIME);
         usedSpeed = originalSpeed;
+        usedTolerance = TOLERANCE;
     }
 
     @Override
     public void execute() {
         if ((pitchAngle * swerve.getPigeonGyro().getRoll() * (forwards ? -1 : 1)) < 0) {
-            usedSpeed = usedSpeed * USEDSPEED_FACTOR;
+            usedTolerance = FINISH_TOLERANCE;
+            usedSpeed = originalSpeed * USEDSPEED_FACTOR;
         }
         pitchAngle = swerve.getPigeonGyro().getRoll() * (forwards ? -1 : 1);//gyro is flipped
-        if (Math.abs(pitchAngle) > TOLERANCE) {
+        if (Math.abs(pitchAngle) > usedTolerance) {
             swerve.moveByChassisSpeeds(usedSpeed * Math.signum(pitchAngle), 0.0, 0.0, 0);
         } else {
             swerve.stop();
@@ -43,7 +49,7 @@ public class BangBangBalance extends SwerveCommand {
 
     @Override
     public boolean isFinished() {
-        boolean debounced = debouncer.calculate(Math.abs(pitchAngle) < TOLERANCE);
+        boolean debounced = debouncer.calculate(Math.abs(pitchAngle) < usedTolerance);
         return super.isFinished() || debounced;
     }
 
