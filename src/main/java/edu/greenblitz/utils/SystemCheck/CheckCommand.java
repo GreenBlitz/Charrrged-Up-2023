39a42ddd950bf038a1b47ef6f8199223f244ed49
NoamRosenberg08 <noamrosenberg097@gmail.com>
2023-05-13 15:02:06 +0300

    noam - added checkCommand and shuffleboard tab

---
+++ src/main/java/edu/greenblitz/utils/SystemCheck/CheckCommand.java
@@ -0,0 +1,56 @@
+package edu.greenblitz.utils.SystemCheck;
+
+import edu.greenblitz.tobyDetermined.RobotMap;
+import edu.greenblitz.tobyDetermined.commands.telescopicArm.goToPosition.GoToPosition;
+import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.Extender;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import java.util.function.BooleanSupplier;
+
+public class CheckCommand {
+
+
+    static {
+        new CheckCommand(new GoToPosition(RobotMap.TelescopicArm.PresetPositions.CONE_HIGH),
+                new BooleanSupplier() {
+                    @Override
+                    public boolean getAsBoolean() {
+                        return Extender.getInstance().isAtLength();
+                    }
+                }
+
+        );
+    }
+
+    private BooleanSupplier isAtTargetSupplier;
+    private Command runCommand;
+
+    private double timeout;
+
+    public CheckCommand(Command runCommand, BooleanSupplier isAtTargetSupplier, double timeout) {
+        this.isAtTargetSupplier = isAtTargetSupplier;
+        this.timeout = timeout;
+        this.runCommand = runCommand.raceWith(new WaitCommand(this.timeout));
+
+    }
+
+    public CheckCommand(Command runCommand, BooleanSupplier isAtTargetSupplier) {
+
+        this.timeout = RobotMap.General.SystemCheckConstants.DEFAULT_TIMEOUT_FOR_CHECK_COMMAND;
+
+        this.isAtTargetSupplier = isAtTargetSupplier;
+        this.runCommand = runCommand.raceWith(new WaitCommand(this.timeout));
+    }
+
+    public boolean IsAtTarget() {
+        return isAtTargetSupplier.getAsBoolean();
+    }
+    public BooleanSupplier getBooleanSupplier (){
+        return this.isAtTargetSupplier;
+    }
+
+    public Command getRunCommand() {
+        return runCommand;
+    }
+}
--- src/main/java/edu/greenblitz/utils/SystemCheck/SystemCheck.java
+++ src/main/java/edu/greenblitz/utils/SystemCheck/SystemCheck.java
@@ -1,16 +1,26 @@
 package edu.greenblitz.utils.SystemCheck;
 
+import edu.greenblitz.tobyDetermined.RobotMap;
+import edu.greenblitz.tobyDetermined.subsystems.Battery;
 import edu.greenblitz.tobyDetermined.subsystems.GBSubsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
+import scala.collection.parallel.immutable.ParRange;
 
 import javax.swing.plaf.PanelUI;
 import java.util.HashMap;
+import java.util.function.DoubleSupplier;
 
-public class SystemCheck {
+public class SystemCheck extends GBSubsystem{
 
-    private HashMap<GBSubsystem, Command> subsystemsAndCommands;
+    private HashMap<GBSubsystem, CheckCommand> subsystemsAndCommands;
     private static SystemCheck instance;
 
+
+    private ShuffleboardTab tab;
+    private double innerBatteryResistance;
+    private double startingVoltage;
     public static SystemCheck getInstance(){
         if (instance == null){
             instance = new SystemCheck();
@@ -19,18 +29,52 @@ public class SystemCheck {
     }
 
     public SystemCheck(){
+
+        this.innerBatteryResistance = (this.startingVoltage - Battery.getInstance().getCurrentVoltage()) / Battery.getInstance().getCurrentUsage();
+
+        this.startingVoltage = 13.1;
+
         subsystemsAndCommands = new HashMap<>();
+
+        this.tab = Shuffleboard.getTab("System check");
     }
 
-    public void add (GBSubsystem subsystem,Command command){
-        subsystemsAndCommands.putIfAbsent(subsystem, command);
+    @Override
+    public void periodic() {
+
+        for (CheckCommand checkCommand : this.subsystemsAndCommands.values()){
+            tab.addBoolean(checkCommand.getRunCommand().getRequirements().getClass().getName(),checkCommand.getBooleanSupplier());
+        }
+
+        tab.addDouble("battery inner resistance:", this::getInnerBatteryResistance);
+        tab.addDouble("battery voltage drop:", new DoubleSupplier() {
+            @Override
+            public double getAsDouble() {
+                return SystemCheck.getInstance().getStartingVoltage() - Battery.getInstance().getCurrentVoltage();
+            }
+        });
+
+
+    }
+
+    public double getInnerBatteryResistance() {
+        return innerBatteryResistance;
+    }
+
+    public double getStartingVoltage() {
+        return startingVoltage;
+    }
+
+    public void add (GBSubsystem subsystem, CheckCommand checkCommand){
+        subsystemsAndCommands.putIfAbsent(subsystem, checkCommand);
+        this.tab.addBoolean(checkCommand.getRunCommand().getRequirements().getClass().getName(),checkCommand.getBooleanSupplier());
     }
 
     public void remove (GBSubsystem subsystem,Command command){
         subsystemsAndCommands.remove(subsystem,command);
     }
 
-    public Command getCommandForSubsystem(GBSubsystem subsystem){
+    public CheckCommand getCheckCommandForSubsystem(GBSubsystem subsystem){
         return subsystemsAndCommands.get(subsystem);
     }
     public GBSubsystem[] getSubsystems (){
