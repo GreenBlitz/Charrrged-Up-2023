3316bff2454b69dac72eb9d6f6a620054151fc81
itamaroryan <itamar.oryan@gmail.com>
2023-08-20 11:47:02 +0300

    Romy - UNCOMITTED CODE, yoav i kill you

---
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/telescopicArm/StraightLineMotion.java
@@ -0,0 +1,15 @@
+package edu.greenblitz.tobyDetermined.commands.telescopicArm;
+
+import edu.greenblitz.utils.GBCommand;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+
+public class StraightLineMotion extends GBCommand {
+
+	private ProfiledPIDController pidController;
+	
+	@Override
+	public void initialize() {
+		pidController.
+	}
+}
+
