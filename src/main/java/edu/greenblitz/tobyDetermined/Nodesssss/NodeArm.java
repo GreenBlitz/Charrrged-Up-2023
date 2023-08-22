9b67529982308f0c841ea69f9eedab61b8bc8a29
YHerm <0549405422.yh@gmail.com>
2023-08-22 13:40:17 +0300

    Yoav - loop problem , commands problem

--- src/main/java/edu/greenblitz/tobyDetermined/Nodesssss/NodeArm.java
+++ src/main/java/edu/greenblitz/tobyDetermined/Nodesssss/NodeArm.java
@@ -8,50 +8,38 @@ import java.util.LinkedList;
 
 public class NodeArm {
     private NodeArm parent;
-    private GBCommand command;
     private int id;
-    private LinkedList<NodeArm> neighbors;
-    private double anglePos;
-    private double extendPos;
-    public NodeArm(GBCommand command, int id, double x, double y){
+    private final LinkedList<NodeArm> neighbors;
+    private final double anglePos;
+    private final double extendPos;
+    public NodeArm(int id, double x, double y){
         this.id = id;
         extendPos = x;
         anglePos = y;
-        this.command = command;
         neighbors = new LinkedList<>();
     }
-
     public NodeArm getParent(){
         return parent;
     }
-
     public void setParent(NodeArm parent) {
         this.parent = parent;
     }
-
     public double getExtendPos() {
         return extendPos;
     }
     public double getAnglePos() {
         return anglePos;
     }
-
     public void setNeighbors(NodeArm[] neighbors) {
         Collections.addAll(this.neighbors, neighbors);
     }
-
     public LinkedList<NodeArm> getNeighbors(){
         return neighbors;
     }
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-    //public void runCommand(){
-        //command.schedule();
-    //}
-
 }
--- src/main/java/edu/greenblitz/tobyDetermined/commands/NodeAllCommand.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/NodeAllCommand.java
@@ -4,25 +4,50 @@ import edu.greenblitz.tobyDetermined.Nodesssss.NodeArm;
 import edu.greenblitz.tobyDetermined.Nodesssss.NodeBase;
 import edu.greenblitz.tobyDetermined.Nodesssss.aStar;
 import edu.greenblitz.utils.GBCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import java.util.LinkedList;
 
 public class NodeAllCommand extends GBCommand {
-    NodeCommand nodeCommand;
     NodeBase nodeBase;
-    int start;
-    int end;
-    public NodeAllCommand(NodeArm nodeArm, int end){
+    NodeArm start;
+    NodeArm end;
+    NodeCommand[] nodeCommands;
+    public NodeAllCommand(NodeArm nodeArm, NodeArm end){
         nodeBase = NodeBase.getInstance();
-        start = nodeArm.getId();
+        start = nodeArm;
         this.end = end;
     }
 
     @Override
-    public void execute() {
-        LinkedList<Integer> path = aStar.getPath(nodeBase.getNode(start),nodeBase.getNode(end));
-        for (int i = 0; i<path.size(); i++){
-            //path.get(i)
+    public void initialize() {
+        LinkedList<NodeArm> path = aStar.getPath(start,end);
+        assert path != null;
+        nodeCommands = new NodeCommand[path.size()-1];
+        for(int i = 0; i<path.size(); i++){
+            nodeCommands[i] = new NodeCommand(path.get(i), path.get(i+1));
         }
     }
+
+    @Override
+    public void execute() {
+            new SequentialCommandGroup(
+                    nodeCommands[0],
+                    nodeCommands[1],
+                    nodeCommands[2]
+            ).schedule();
+    }
+
+    @Override
+    public boolean isFinished() {
+        // if in point
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        //stop motors
+        nodeBase.setCurrentNode(end);
+    }
 }
+
