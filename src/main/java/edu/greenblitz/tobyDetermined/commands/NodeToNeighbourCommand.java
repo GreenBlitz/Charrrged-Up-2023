76a1dc9b1d7293d17b1602b9240d418d8a3df0e4
YoniKiriaty <yonikiriaty@gmail.com>
2023-09-21 13:45:38 +0300

    Yoni - deleted all unnecessary functions and variables in node to neighbor

--- src/main/java/edu/greenblitz/tobyDetermined/commands/NodeToNeighbourCommand.java
+++ src/main/java/edu/greenblitz/tobyDetermined/commands/NodeToNeighbourCommand.java
@@ -8,9 +8,6 @@ import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.Extender;
 import edu.greenblitz.utils.GBCommand;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.opencv.core.Mat;
-
-import java.util.ArrayList;
 
 public class NodeToNeighbourCommand extends GBCommand {
     private final Extender extender;
@@ -20,9 +17,6 @@ public class NodeToNeighbourCommand extends GBCommand {
     private double COMBINED_VELOCITY = 0.2;
     private double MAX_EXTENDER_VELOCITY = 0.2; //In Meters Per Second
     private double MAX_ANGULAR_VELOCITY = Units.degreesToRadians(200);
-    private double[][] littlePoints;
-    private ExtendToLength curCommand;
-    private int miniIndex = 1;
 
     public NodeToNeighbourCommand(NodeArm start, NodeArm end){
         extender = Extender.getInstance();
@@ -42,51 +36,7 @@ public class NodeToNeighbourCommand extends GBCommand {
         double beta = Math.asin(a*Math.sin(gamma)/c);
         return Math.tan(beta);
     }
-    public static double[][] createPoints(double x1, double y1, double x2, double y2) {
-        double distance = Math.sqrt((y1-y2)*(y1-y2)+(x1-x2)*(x1-x2));
-        double slope = (y2-y1)/(x2-x1);
-        double yIntercept = y2-x2*slope;
-        int n = 3;//to be changed
-        int numOfPoints = (int)distance/n;
-        ArrayList<Double> x = new ArrayList<>();
-        ArrayList<Double> y = new ArrayList<>();
-        if(x1>x2)
-            n*=-1;
-        for(int i = 0; i< numOfPoints; i++) {
-            x.add(x1);
-            y.add(x1*slope+yIntercept);
-            x1+=n;
-        }
-        x.add(x2);
-        y.add(x2*slope+yIntercept);
-        double[][] tablePoints = new double[2][x.size()];
-        double[] polarRepresentation;
-        for (int i = 0; i < x.size(); i++) {
-            polarRepresentation = convertToPolar(x.get(i),y.get(i));
-            tablePoints[0][i] = polarRepresentation[0];
-            tablePoints[1][i] = polarRepresentation[1];
-        }
-        return tablePoints;
-    }
-    public static double[] convertToCartesian(double radius, double angle) {
-        // Calculate the Cartesian coordinates
-        double x = radius * Math.cos(angle);
-        double y = radius * Math.sin(angle);
 
-        return new double[] { x, y };
-    }
-    public static double[] convertToPolar(double x,double y) {
-        double radius = Math.sqrt(x*x+y*y);
-        double angle = Math.atan2(y,x);
-        return new double[] {radius,angle};
-    }
-    public void moveLittlePoints(NodeArm nodeEndIndex){
-        double[] currentCords = convertToCartesian(extender.getLength(),elbowSub.getAngleRadians());
-        double[] endNodeCords = convertToCartesian(nodeEndIndex.getExtendPos(),nodeEndIndex.getAnglePos());
-        littlePoints = createPoints(currentCords[0],currentCords[1],endNodeCords[0],endNodeCords[1]);//polar form
-
-
-    }
     public double calculateExtenderVelocity(double ratio, NodeArm nodeEndIndex) {
         double signOfExtender = Math.signum(nodeEndIndex.getExtendPos()-extender.getLength());
         double extenderVelocity = Math.sqrt(COMBINED_VELOCITY * COMBINED_VELOCITY /(ratio*ratio+1));
@@ -141,11 +91,6 @@ public class NodeToNeighbourCommand extends GBCommand {
             moveArm(end);
         }
 
-//        if (curCommand.isFinished())
-//        elbowSub.moveTowardsAngleRadians(littlePoints[1][miniIndex]);
-//        if(littlePoints[0][miniIndex] != curCommand.getLengthGoal()){
-//            elbowSub.moveTowardsAngleRadians(littlePoints[1][miniIndex]);
-//        }
     }
 
     @Override
