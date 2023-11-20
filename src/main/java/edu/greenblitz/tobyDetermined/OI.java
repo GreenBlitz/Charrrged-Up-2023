cc55f0d13342d2beff6ea44d2479d3422c909876
GreenBlitz <greenblitz4590@gmail.com>
2023-11-20 19:32:44 +0200

    Amir and romy - rebase shanniges

--- src/main/java/edu/greenblitz/tobyDetermined/OI.java
+++ src/main/java/edu/greenblitz/tobyDetermined/OI.java
@@ -1,5 +1,4 @@
 package edu.greenblitz.tobyDetermined;
-
 import edu.greenblitz.tobyDetermined.commands.Auto.balance.LockWheels;
 import edu.greenblitz.tobyDetermined.commands.Auto.balance.bangBangBalance.FullBalance;
 import edu.greenblitz.tobyDetermined.commands.MultiSystem.FullIntake;
@@ -31,6 +30,7 @@ import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.Extender;
 import edu.greenblitz.tobyDetermined.subsystems.telescopicArm.ObjectSelector;
 import edu.greenblitz.utils.hid.SmartJoystick;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class OI { //GEVALD
 
@@ -67,7 +67,6 @@ public class OI { //GEVALD
 	public static void disableHandling() {
 		isHandled = false;
 	}
-
 
 	private void initButtons() {
 		SwerveChassis.getInstance().setDefaultCommand(new CombineJoystickMovement(true));
