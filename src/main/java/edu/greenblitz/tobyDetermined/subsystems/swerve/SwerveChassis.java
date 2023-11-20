c07d9cef4775eec79d11524a360137283b784b0c
GreenBlitz <greenblitz4590@gmail.com>
2023-11-20 19:25:39 +0200

    Ran Gil and Ben and Jeris-setting start point

--- src/main/java/edu/greenblitz/tobyDetermined/subsystems/swerve/SwerveChassis.java
+++ src/main/java/edu/greenblitz/tobyDetermined/subsystems/swerve/SwerveChassis.java
@@ -18,7 +18,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.*;
 import edu.wpi.first.util.sendable.Sendable;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -32,7 +31,6 @@ public class SwerveChassis extends GBSubsystem {
 	private final SwerveModule frontRight, frontLeft, backRight, backLeft;
 	private final PigeonGyro pigeonGyro;
 	private final SwerveDriveKinematics kinematics;
-	private final BuiltInAccelerometer accelerometer;
 
 
 	private final SwerveDriveOdometry odometry;
@@ -73,7 +71,6 @@ public class SwerveChassis extends GBSubsystem {
 				new MatBuilder<>(Nat.N3(), Nat.N1()).fill(RobotMap.Vision.STANDARD_DEVIATION_VISION2D, RobotMap.Vision.STANDARD_DEVIATION_VISION2D, RobotMap.Vision.STANDARD_DEVIATION_VISION_ANGLE));
 		SmartDashboard.putData("field", getField());
 		SmartDashboard.putData("field", getField());
-		accelerometer = new BuiltInAccelerometer();
 	}
 	
 	
